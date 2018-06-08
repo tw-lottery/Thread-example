fd93a31d2414995f4641cd1190ea9132b6ce3ea0
Honglai <hlzhan@thoughtworks.com>
2018-06-08 08:57:26 +0800

    [Honglai] #N/A : use Semaphore to implement

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
-import java.util.concurrent.Executor;
+import java.util.concurrent.Semaphore;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Executor executor = Runnable::run;
-        MyRunnable print1 = new MyRunnable("A");
-        MyRunnable print2 = new MyRunnable("B");
-        MyRunnable print3 = new MyRunnable("C");
+    private static final Semaphore semaphore1 = new Semaphore(1);
+    private static final Semaphore semaphore2 = new Semaphore(1);
+    private static final Semaphore semaphore3 = new Semaphore(1);
 
-        MyScheduler scheduler = new MyScheduler(executor);
+    public static void main(String[] args) throws InterruptedException {
+        semaphore2.acquire();
+        semaphore3.acquire();
 
-        for (int i=0; i < 100; i ++) {
-            scheduler.addRunner(print1);
-            scheduler.addRunner(print2);
-            scheduler.addRunner(print3);
-        }
+        MyRunnable print1 = new MyRunnable("A", semaphore1, semaphore2);
+        MyRunnable print2 = new MyRunnable("B", semaphore2, semaphore3);
+        MyRunnable print3 = new MyRunnable("C", semaphore3, semaphore1);
 
-        scheduler.runNext();
+        Thread thread1 = new Thread(print1);
+        Thread thread2 = new Thread(print2);
+        Thread thread3 = new Thread(print3);
+
+        thread3.start();
+        thread2.start();
+        thread1.start();
     }
 
 }
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -1,12 +1,27 @@
+import java.util.Objects;
+import java.util.concurrent.Semaphore;
+
 public class MyRunnable implements Runnable {
     private String content;
+    private Semaphore acquireSemaphore;
+    private Semaphore releaseSemaphore;
 
-    public MyRunnable(String content) {
+    public MyRunnable(String content, Semaphore acquireSemaphore, Semaphore releaseSemaphore) {
         this.content = content;
+        this.acquireSemaphore = acquireSemaphore;
+        this.releaseSemaphore = releaseSemaphore;
     }
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " content is : " + this.content);
+        while (Objects.nonNull(content)) {
+            try {
+                acquireSemaphore.acquire();
+                System.out.println(content);
+                releaseSemaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
